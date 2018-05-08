5785dbad5f44e0de4c7e5385c4228b43ab75a514
JakeTheReaper <jake.zerafa86@gmail.com>
2018-05-08 12:41:30 +1000

    Updated Code - Includes Classes

--- ifttt.py
+++ ifttt.py
@@ -20,54 +20,56 @@ else:
     print(filename + ": opened successfully")
     fh.close()
 
-def timeofday():
-    now = datetime.datetime.now()
-    datastore['services']['time of day']['parameters'] = now.strftime('%d/%m/%Y %H:%M:%S')
-    date = datastore['services']['time of day']['parameters']
-    return date
+class Services():
+    def timeofday():
+        now = datetime.datetime.now()
+        datastore['services']['time of day']['parameters'] = now.strftime('%d/%m/%Y %H:%M:%S')
+        date = datastore['services']['time of day']['parameters']
+        return date
 
-def geolocation():
-    command = datastore['services']['geolocation']['program']
-    p = run(command, stdout=PIPE, input='', encoding='utf-8')
-    return p.stdout
+    def geolocation():
+        command = datastore['services']['geolocation']['program']
+        p = run(command, stdout=PIPE, input='', encoding='utf-8')
+        return p.stdout
 
-def echo():
-    echo = datastore['services']['echo']['parameters']
-    return echo
+    def echo():
+        echo = datastore['services']['echo']['parameters']
+        return echo
 
-def appendTimeOfDay():
-    try:
-        fh = "times.txt"
-        file = open(fh, "w")
-    except IOError:
-        print("Error: File not found")
-        sys.exit(0)
-    else:
-        for flow in datastore['flows']['Append time of day']:
-            if flow == 'time of day':
-                pEcho = re.sub(r'\$\$', flow, echo())
-                print(pEcho)
-                file.write(str(timeofday())+'\n')
-                time.sleep(1)
-            elif flow == 'geolocation':
-                pEcho = re.sub(r'\$\$', flow, echo())
-                print(pEcho)
-                file.write(str(geolocation()))
-                time.sleep(1)
-            elif flow == 'append to file':
-                pEcho = re.sub(r'\$\$', flow, echo())
-                print(pEcho)
-                time.sleep(1)
-            else:
-                print(flow + ": Is Not a Valid Service - SKIPPING...")
-                time.sleep(1)
-                pass
+class Flows():
+    def appendTimeOfDay():
+        try:
+            fh = "times.txt"
+            file = open(fh, "w")
+        except IOError:
+            print("Error: File not found")
+            sys.exit(0)
+        else:
+            for flow in datastore['flows']['Append time of day']:
+                if flow == 'time of day':
+                    pEcho = re.sub(r'\$\$', flow, Services.echo())
+                    print(pEcho)
+                    file.write(str(Services.timeofday())+'\n')
+                    time.sleep(1)
+                elif flow == 'geolocation':
+                    pEcho = re.sub(r'\$\$', flow, Services.echo())
+                    print(pEcho)
+                    file.write(str(Services.geolocation()))
+                    time.sleep(1)
+                elif flow == 'append to file':
+                    pEcho = re.sub(r'\$\$', flow, Services.echo())
+                    print(pEcho)
+                    time.sleep(1)
+                else:
+                    print(flow + ": Is Not a Valid Service - SKIPPING...")
+                    time.sleep(1)
+                    pass
 
-        print("Data written to " + fh + " successfully")
-        file.close()
+            print("Data written to " + fh + " successfully")
+            file.close()
 
 def main():
-    appendTimeOfDay()
+    Flows.appendTimeOfDay()
     sys.exit(0)
 
 main()
