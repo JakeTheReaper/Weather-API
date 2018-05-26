4d9a3e1f763a7f0beef317800ad7f2d4335f7dab
JakeTheReaper <jake.zerafa86@gmail.com>
2018-05-26 12:55:33 +1000

    Updated Code

--- Part_01/ifttt.py
+++ Part_01/ifttt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+#Student Name: Jake Duran Zerafa
+#Student No: s3679109
+
 import datetime
 import requests
 import json
@@ -8,35 +11,53 @@ from subprocess import run, PIPE
 import time
 import re
 
+#Assign default json file to be read in.
 filename = "ifttt.json"
 
+#Try open assigned default json file and save contents to a variable.
+#If file not found, print error and exit process.
+#Otherwise close file.
 try:
     fh = open(filename, 'r')
     datastore = json.load(fh)
 except IOError:
     print("Error: File not found")
-    sys.exit(0)
+    sys.exit(1)
 else:
-    print(filename + ": opened successfully")
+    #print(filename + ": opened successfully")
     fh.close()
 
+#Create Services class.
 class Services():
+    
     def timeofday():
+        #Get the time of day, format and return the output. 
         now = datetime.datetime.now()
         datastore['services']['time of day']['parameters'] = now.strftime('%d/%m/%Y %H:%M:%S')
         date = datastore['services']['time of day']['parameters']
         return date
 
     def geolocation():
-        command = datastore['services']['geolocation']['program']
-        p = run(command, stdout=PIPE, input='', encoding='utf-8')
-        return p.stdout
+        #Try open program to get location metadata
+        #If file can't be opened, print error and exit process.
+        #Otherwise return output
+        try:
+            command = datastore['services']['geolocation']['program']
+            p = run(command, stdout=PIPE, input='', encoding='utf-8')
+        except IOError:
+            print("Error: Cannot open file")
+            sys.exit(1)
+        else:
+            return p.stdout
 
     def echo():
+        #Echo current flow name.
         echo = datastore['services']['echo']['parameters']
         return echo
 
     def morning():
+        #Check to see if service is run in the morning.
+        #Get current time and return True or False if it is within the specified parameters.
         now = datetime.datetime.now()
         datastore['services']['morning']['parameters'] = now.strftime('%H:%M:%S')
         time = datastore['services']['morning']['parameters']
@@ -46,6 +67,8 @@ class Services():
             return False         
     
     def afternoon():
+        #Check to see if service is run in the afternoon.
+        #Get current time and return True or False if it is within the specified parameters.
         now = datetime.datetime.now()
         datastore['services']['morning']['parameters'] = now.strftime('%H:%M:%S')
         time = datastore['services']['morning']['parameters']
@@ -55,6 +78,8 @@ class Services():
             return False
 
     def evening():
+        #Check to see if service is run in the evening.
+        #Get current time and return True or False if it is within the specified parameters.
         now = datetime.datetime.now()
         datastore['services']['morning']['parameters'] = now.strftime('%H:%M:%S')
         time = datastore['services']['morning']['parameters']
@@ -64,9 +89,18 @@ class Services():
             return False
 
     def sunrise():
-        command = datastore['services']['sunrise']['program']
-        run(command, stdout=PIPE, input='', encoding='utf-8')
-        
+        #Try open program to get sunrise metadata
+        #If file can't be opened, print error and exit process.
+        try:
+            command = datastore['services']['sunrise']['program']
+            run(command, stdout=PIPE, input='', encoding='utf-8')
+        except IOError:
+            print("Error: Cannot open file")
+            sys.exit(1)
+
+        #Try open json file with sunrise results.
+        #If file can't be opened, print error and exit process.
+        #Otherwise close file.
         filename = "sunriseSunset.json"
         try:
             fh = open(filename, 'r')
@@ -77,13 +111,23 @@ class Services():
         else:
             fh.close()
 
+        #Assign sunrise result and return output.
         sunrise = results['results']['sunrise']
         return sunrise
         
     def sunset():
-        command = datastore['services']['sunset']['program']
-        run(command, stdout=PIPE, input='', encoding='utf-8')
-        
+        #Try open program to get sunset metadata
+        #If file can't be opened, print error and exit process.
+        try:
+            command = datastore['services']['sunset']['program']
+            run(command, stdout=PIPE, input='', encoding='utf-8')
+        except IOError:
+            print("Error: Cannot open file")
+            sys.exit(1)
+
+        #Try open json file with sunset results.
+        #If file can't be opened, print error and exit process.
+        #Otherwise close file.
         filename = "sunriseSunset.json"
         try:
             fh = open(filename, 'r')
@@ -94,9 +138,11 @@ class Services():
         else:
             fh.close()
 
+        #Assign sunset result and return output.
         sunset = results['results']['sunset']
         return sunset
 
+#Create Flows class.
 class Flows():
     def appendTimeOfDay():
         try:
@@ -136,7 +182,7 @@ class Flows():
                     time.sleep(1)
                     pass
 
-            print("Data written to " + fh + " successfully")
+            print("Data written successfully")
             file.close()
 
 def main():
